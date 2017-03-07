704a714e63c0d753d8e5cbeea7f9cbcd0cdc817f
Erik Bråhammar <erik.brahammar@consid.se>
2017-03-07 13:11:49 +0100

    Epi 101

---
+++ FredriksEpiPartOne/FredriksEpiPartOne/Controllers/TestPageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using FredriksEpiPartOne.Models.Pages;
+
+namespace FredriksEpiPartOne.Controllers
+{
+    public class TestPageController : PageController<TestPage>
+    {
+        public ActionResult Index(TestPage currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(currentPage);
+        }
+    }
+}
---
+++ FredriksEpiPartOne/FredriksEpiPartOne/Models/Pages/TestPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.ChangeLog;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace FredriksEpiPartOne.Models.Pages
+{
+    [ContentType(DisplayName = "TestPage", GUID = "564163c4-1e07-4086-bb52-ef35bd7cdd1d", Description = "")]
+    public class TestPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Namnet i editorn",
+            Description = "Lite hjälptext eller så.",
+            GroupName = "Grupp 1",
+            Order = 10
+            )]
+        public virtual string Header { get; set; }
+    }
+}
